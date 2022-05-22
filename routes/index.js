0a11c22022527830c2c88e645c933df01ec7471e
TheThinkPotato <dan.g.lopez@outlook.com>
2022-05-23 01:54:02 +1000

    :sparkles: Added all routes working.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-var express = require("express");
-const { set } = require("express/lib/application");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const secretKey = "SUPER SECRET KEY DO NOT STEAL";
 
+router.get("/me", function (req, res, next) {
+  res.status(200).json(
+    {
+      "name": "Daniel Lopez",
+      "student_number": "n10956611"
+    }
+  )
+})
 
 
 router.get("/countries", function (req, res, next) {
@@ -12,16 +19,16 @@ router.get("/countries", function (req, res, next) {
     .from("data")
     .select("country")
     .then((rows) => {
-       
+
       const countrySet = new Set();
-      
+
       rows.forEach(element => {
         countrySet.add(element.country);
       });
-      
+
       const countryList = [...countrySet].sort();
-      res.json( countryList        
-      )      
+      res.json(countryList
+      )
     })
     .catch((err) => {
       console.log(err);
@@ -30,85 +37,113 @@ router.get("/countries", function (req, res, next) {
 });
 
 // volcanoes?country=japan&populatedWithin=5km 
+router.get("/:volcanoes", function (req, res, next) {
+  let setDistance;
 
-router.get("/:volcanoes", function (req, res, next) {  
+  if (req.query.country) {
 
-  const input = req.originalUrl  
-
-  const check = input.slice(1,input.indexOf("?"));  
-
-  if(check === "volcanoes" & input.indexOf("&") > 0 )
-  {
-  const country = input.slice(input.indexOf("=")+1,input.indexOf("&"));
-  const distance = input.slice(input.lastIndexOf("=")+1);
-  let setDistance = "population_5km"
-
-  if(distance === "5km")
-  {
-    setDistance = "population_5km";
+    if (req.query.populatedWithin) {
+      if (req.query.populatedWithin === "5km") {
+        setDistance = "population_5km";
+      }
+      else if (req.query.populatedWithin === "10km") {
+        setDistance = "population_10km";
+      }
+      else if (req.query.populatedWithin === "30km") {
+        setDistance = "population_10km";
+      }
+      else if (req.query.populatedWithin === "100km") {
+        setDistance = "population_100km";
+      }
+      else {
+        res.json({ Error: true, Message: "Invalid value for populatedWithin: 15km. Only: 5km,10km,30km,100km are permitted." });
+        return;
+      }
+      req.db
+        .from("data")
+        .select("id", "name", "country", "region", "subregion")
+        .where("country", "=", req.query.country)
+        .where(setDistance, "!=", 0)
+        .then((rows) => {
+          res.json(rows);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json({ Error: true, Message: "Invalid query parameters. Query parameters are not permitted." });
+        });
+    } else {
+      req.db
+        .from("data")
+        .select("id", "name", "country", "region", "subregion")
+        .where("country", "=", req.query.country)
+        .then((rows) => {
+          res.json(rows);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json({ Error: true, Message: "Invalid query parameters. Query parameters are not permitted." });
+        });
+    }
+  } else {
+    res.status(400).json({ Error: true, Message: "Country is a required query parameter." });
   }
-  else if(distance === "10km")
-  {
-    setDistance = "population_10km";
-  }
-  else if(distance === "30km")
-  {
-    setDistance = "population_10km";
-  }
-  else if(distance === "100km")
-  {
-    setDistance = "population_100km";
-  }
-  else{
-    res.json({ Error: true, Message: "Invalid value for populatedWithin: 15km. Only: 5km,10km,30km,100km are permitted." });
-    return;
-  }
-  req.db
-    .from("data")    
-    .select("id" ,"name","country","region","subregion")
-    .where("country", "=", country)
-    .where(setDistance, "!=", 0)
-    .then((rows) => {
-      res.json(rows);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ Error: true, Message: "Invalid query parameters. Query parameters are not permitted." });
-    });
-
-  }else if (check === "volcanoes"){
-    const country = input.slice(input.indexOf("=")+1)
-    req.db
-    .from("data")
-    .select("id" ,"name","country","region","subregion")    
-    .where("country", "=", country)
-    .then((rows) => {
-      res.json(rows);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ Error: true, Message: "Invalid query parameters. Query parameters are not permitted." });
-    });
-
-  }
-  else{      
-    res.json({ Error: true, Message: "Invalid query parameters. Query parameters are not permitted." });    
-  }
-  
 });
 
-router.get("/api/city/:CountryCode", function (req, res, next) {
+// check is current token is valid
+// @param auth takes req.headers.authorization
+const authCheck = function (auth) {
+  let response = false;
+  if (!auth || auth.split(" ").length !== 2) {
+    response = false;
+  } else {
+    const token = auth.split(" ")[1];
+    try {      
+      const payload = jwt.verify(token, secretKey);
+      if (Date.now() > payload.exp) {
+        response = false;
+        return;
+      }      
+      response = true;
+    } catch (e) {      
+      response = false;
+      return;
+    }
+  }
+  return response
+}
+
+
+router.get("/volcano/:id", function (req, res, next) {
+  let selectSQL = ["id", "name", "country", "region", "subregion", "last_eruption", "summit", "elevation", "latitude", "longitude"];
+  if (req.headers.authorization) {
+    let auth = authCheck(req.headers.authorization);
+    if (auth) {
+      selectSQL = "*"
+    } else {
+      res.status(401).json({
+        Error: true,
+        Message: "Invalid JWT token"
+      });
+      return;
+    }
+
+  }
   req.db
-    .from("city")
-    .select("*")
-    .where("CountryCode", "=", req.params.CountryCode)
+    .from("data")
+    .select(selectSQL)
+    .where("id", "=", req.params.id)
     .then((rows) => {
-      res.json({ Error: false, Message: "Success", Cities: rows });
+      if (rows.length === 0) {
+        res.json({ Error: true, Message: "Volcano with ID: " + req.params.id + " not found." });
+        return;
+      }
+      res.json(rows);
     })
     .catch((err) => {
       console.log(err);
-      res.json({ Error: true, Message: "Error executing MySQL query" });
+      res.json({ Error: true, Message: "Invalid query parameters. Query parameters are not permitted." });
     });
+
 });
 
 router.post('/register', function (req, res, next) {
@@ -133,7 +168,7 @@ router.post('/register', function (req, res, next) {
       if (users.length > 0) {
         res.status(409).json({
           Error: true,
-          Message: "Email already in use"
+          Message: "User already exists"
         });
         return;
       }
@@ -175,9 +210,9 @@ router.post('/login', function (req, res, next) {
   req.db.from("users").select("*").where({ email })
     .then(users => {
       if (users.length === 0) {
-        res.status(400).json({
+        res.status(401).json({
           Error: true,
-          Message: "User not registered"
+          Message: "Incorrect email or password"
         });
         return;
       }
@@ -185,9 +220,9 @@ router.post('/login', function (req, res, next) {
       const { hash } = users[0];
 
       if (!bcrypt.compareSync(password, hash)) {
-        res.status(400).json({
+        res.status(401).json({
           Error: true,
-          Message: "Incorrect password"
+          Message: "Incorrect email or password"
         });
         return;
       }
@@ -196,7 +231,7 @@ router.post('/login', function (req, res, next) {
       const exp = Date.now() + expires_in * 1000;
       const token = jwt.sign({ email, exp }, secretKey);
 
-      res.status(200).json({ 
+      res.status(200).json({
         token,
         token_type: "Bearer",
         expires_in
